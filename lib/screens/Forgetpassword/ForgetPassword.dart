c0a3722ded32e4ab17aebc655fbb114420450dc5
sahan chamod <sahan7130@gmail.com>
2024-10-16 00:43:15 +0530

    screens

---
+++ lib/screens/Forgetpassword/ForgetPassword.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ForgetPassword extends StatefulWidget {
+  const ForgetPassword({super.key});
+
+  @override
+  State<ForgetPassword> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<ForgetPassword> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/widget/constant.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const kmainbackgroundcolor = Colors.orange;
+var kbackgroundcolor = Colors.orange.shade100;
+var kfillcolor = Colors.grey.shade200;
